6726d8172737f5e1230cb919277977f30c86dc7a
Michal Sikora <michalsikora90@gmail.com>
2018-12-13 22:43:40 +0100

    Use collection literal for Dagger modules.

--- src/main/kotlin/pl/com/digita/example/di/ApplicationComponent.kt
+++ src/main/kotlin/pl/com/digita/example/di/ApplicationComponent.kt
@@ -3,7 +3,7 @@ package pl.com.digita.example.di
 import dagger.Component
 import pl.com.digita.example.implementations.Application
 
-@Component(modules = arrayOf(DependencyFactories.Companion::class, Bindings::class))
+@Component(modules = [DependencyFactories.Companion::class, Bindings::class])
 interface ApplicationComponent{
     fun makeApplication(): Application
 }
